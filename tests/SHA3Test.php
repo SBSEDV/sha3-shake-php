7ef7167d86011660dd138c64f5ff9878607fd6b5
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2023-04-15 12:01:13 +0200

    Update tests

--- tests/SHA3Test.php
+++ tests/SHA3Test.php
@@ -7,31 +7,6 @@ use SBSEDV\SHA3Shake\SHA3;
 
 class SHA3Test extends TestCase
 {
-    public function testShake(): void
-    {
-        $testCases = [
-            128 => [
-                // output_length, raw_input, expected_output
-                [256, '', '7f9c2ba4e88f827d616045507605853ed73b8093f6efbc88eb1a6eacfa66ef26'],
-                [256, 'This is a SHAKE128 test string.', '88a5aca15e54e1724fd6e191b4694c742eeba3d2433da71188a21dec93a064ed'],
-                [256, 'This is another SHAKE128 test string.', '95a2e0aa878aed2edafee2edc5a9493f39b82a6273fbd5cd820412cf33eb38b0'],
-            ],
-            256 => [
-                [512, '', '46b9dd2b0ba88d13233b3feb743eeb243fcd52ea62b81b82b50c27646ed5762fd75dc4ddd8c0f200cb05019d67b592f6fc821c49479ab48640292eacb3b7c4be'],
-                [512, 'This is a SHAKE256 test string.', '3477218c282909fa2e1df2e7c3bc4e6bb2c935ceaf95c4b421bfb948bcc750b8e7da8f04d85f8bfaf80f5b09c007fd7d43aa0361da1cb79b875d32114b1f9421'],
-                [512, 'This is another SHAKE256 test string.', '1296397e3b6747173f437148309a4309477887e152521e41b6eaa7a160ce890de12ac6153d545c8ace82c84e1ccf9a3813357cdaf3fd4f31ca5bfc2354945084'],
-            ],
-        ];
-
-        foreach ($testCases as $capacity => $x) {
-            foreach ($x as $testcase) {
-                $this->assertEquals(SHA3::shake($testcase[1], $capacity, $testcase[0]), $testcase[2]);
-
-                $this->assertEquals(SHA3::shake($testcase[1], $capacity, $testcase[0], true), \hex2bin($testcase[2]));
-            }
-        }
-    }
-
     /**
      * Test that only "128" and "256" is accepted as capacity.
      */
@@ -40,5 +15,57 @@ class SHA3Test extends TestCase
         $this->expectException(\InvalidArgumentException::class);
 
         SHA3::shake('', 999, 256);
+    }
+
+    /**
+     * Test that only even output lengths are accepted.
+     */
+    public function testShakeOutputLength(): void
+    {
+        SHA3::shake('', 128, 256);
+        SHA3::shake('', 256, 128);
+
+        $this->expectException(\InvalidArgumentException::class);
+        SHA3::shake('', 128, 63);
+
+        $this->expectException(\InvalidArgumentException::class);
+        SHA3::shake('', 256, 57);
+    }
+
+    /**
+     * @dataProvider shakeProvider
+     */
+    public function testShake(string $string, int $capacity, int $outputLength, string $expected): void
+    {
+        $hash = SHA3::shake($string, $capacity, $outputLength, false);
+        $this->assertEquals($hash, $expected);
+
+        $this->assertTrue(\mb_strlen($hash, '8bit') === $outputLength);
+
+        $this->assertEquals(SHA3::shake($string, $capacity, $outputLength, true), \hex2bin($expected));
+    }
+
+    public function shakeProvider(): array
+    {
+        return [
+            [
+                // @see https://bitbucket.org/openid/connect/issues/1125
+                'YmJiZTAwYmYtMzgyOC00NzhkLTkyOTItNjJjNDM3MGYzOWIy9sFhvH8K_x8UIHj1osisS57f5DduL',
+                256, 114,
+                'b01fd4ef68f26f45a0b57f13b15a2a2679ba083dbde56f607d20d1c648a50772c02fb4448bd258bad456fecd72138943f1c39f3197fcde08b8',
+            ],
+            [
+                '', 128, 64, '7f9c2ba4e88f827d616045507605853ed73b8093f6efbc88eb1a6eacfa66ef26',
+            ],
+            [
+                '', 256, 64, '46b9dd2b0ba88d13233b3feb743eeb243fcd52ea62b81b82b50c27646ed5762f',
+            ],
+            [
+                'This is a test string.', 128, 256, '041b3634a7b103979e4c7e100cdc6ec19e3541b15633657ebae4bb7dd9aca0dc3a0ffe4a202f9fadad9ce141b9861f384fb451ffb1fd79396592f14ffbacc1f83a2b8997fd1ab7c91ee16821756f31b8997b6db1f398d9fbb3b5879908b691957439ca7e009a38ba3de4970ea206182d991d9afc8f8d53fc307ace22d60673e5',
+            ],
+            [
+                'This is a test string.', 256, 256, 'ec31f7a681a317a276b844e22e3e777834c5de311816d70329d5c8054846946bf087163bb91c722a221caa336fd187cdc21b4718995cf729706d92d706d0ff8537f7ddea6271c4549451a17f9a133be23a403f2d6447babcb9b21da4f1d2bc5a4c51a12bfe96aa29560d47f545083d1438cbf7034f5dc1e92e9068980969b01b',
+            ],
+        ];
     }
 }
