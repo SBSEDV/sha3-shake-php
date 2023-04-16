c7851369b9466367ce2c8780402f24825130917d
Daniel Burger <48986191+danielburger1337@users.noreply.github.com>
2023-04-16 19:33:14 +0200

    Update License

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-use danielburger1337\SHA3Shake\SHA3Shake;
-
-require 'src/SHA3Shake.php';
-
-$hash = SHA3Shake::shake256('This is a test string.', 64);
-
-echo $hash.PHP_EOL;
